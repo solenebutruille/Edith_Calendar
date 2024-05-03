7c748e731d4c41bd6f9b17a0469cdf07a9426384
solenebutruille <47445746+solenebutruille@users.noreply.github.com>
2024-05-03 19:29:02 +0100

    fixing add event modal

--- src/components/CalendarItem.vue
+++ src/components/CalendarItem.vue
@@ -119,7 +119,7 @@
         this.modalData.dates = [date];
         this.modalData.startDate = date;
         this.modalData.endDate = null;
-        this.modalData.title = "";
+        this.modalData.title = selectedParticipant.title;
         this.modalData.newEvent = true;
         this.modalData.color = selectedParticipant.color;
         this.modalData.participant = selectedParticipant.title;
